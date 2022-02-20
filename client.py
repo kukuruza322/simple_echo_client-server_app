b137ea188d10093cd35937ac2ceb9ba7d1e329f5
kukuruza322 <72981160+kukuruza322@users.noreply.github.com>
2022-02-20 16:31:34 +0300

    Some refactoring done.

--- client.py
+++ client.py
@@ -4,17 +4,17 @@ connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 IP = socket.gethostbyname(socket.gethostname())  # check this before run if client-server are not in one PC.
 PORT = 20000
 connection.connect((IP, PORT))
-read_server_response = connection.recv(2048)  # get and print a hello message from server
-print(read_server_response.decode('utf8'))
+print(connection.recv(2048).decode("utf8"))  # get and print a hello message from server
 message = input("Enter you nickname and Press Enter: ")
 connection.send(message.encode('utf8'))
-print("Welcome, " + message + "!")
+print(connection.recv(2048).decode("utf8"))  # get and print a individual greetings from server
 
 while True:
     message = input('Enter your text: ')
+    # print("user : " + message) - works fine but only for 1 user. Проще делать обратную рассылку на всех активных пользователей
+    # кто подключен, иначе в консоли будет дублирование локального сообщения и обработонного, полученного с сервера.
     if message == "exit":
         connection.close()  # close connection after EXIT code
         break
     connection.send(message.encode('utf8'))
-    read_server_response = connection.recv(2048)
-    print(read_server_response.decode('utf8'))
+    print(connection.recv(2048).decode("utf8"))
--- server.py
+++ server.py
@@ -7,7 +7,6 @@ PORT = 20000
 s.bind((IP, PORT))  # Bind the socket to address. The socket must not already be bound.
 s.listen(1)  # Enable a server to accept connections. 1 - maximum of connections permit
 connection, address = s.accept()
-connection.send("Hi ! You are welcome to join us!".encode('utf8'))  # send a greetings to client
 
 
 class User:
@@ -17,9 +16,13 @@ class User:
 
 
 with connection:
+    connection.send("Hi! You are welcome to join us!".encode('utf8'))  # send a greetings to client
     getting_username = connection.recv(2048).decode("utf8")
     user = User(getting_username)
-    print(user.name + ' enter chat. ' + 'Connected by ' + str(address[0]) + '.')  # WTF is address[1] ???
+    greetings = user.name + ' enter chat. ' + 'Connected by ' + str(address[0]) + '.'
+    print(greetings)  # WTF is address[1] ???
+    connection.send(greetings.encode('utf8'))
+
     while True:
         input_message = connection.recv(2048).decode("utf8")
         user.message = input_message
@@ -28,3 +31,4 @@ with connection:
         print(processed_message)
         if not input_message:
             break
+
